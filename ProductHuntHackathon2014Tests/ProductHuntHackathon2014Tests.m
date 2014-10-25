e09289d0b6b3ab09616d91e74d87d60bc3ee2a23
koroket <bmasakazu@gmail.com>
2014-10-25 11:47:50 -0700

    Initial Commit

---
+++ ProductHuntHackathon2014Tests/ProductHuntHackathon2014Tests.m
@@ -0,0 +1,40 @@
+//
+//  ProductHuntHackathon2014Tests.m
+//  ProductHuntHackathon2014Tests
+//
+//  Created by sloot on 10/25/14.
+//  Copyright (c) 2014 sloot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ProductHuntHackathon2014Tests : XCTestCase
+
+@end
+
+@implementation ProductHuntHackathon2014Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
